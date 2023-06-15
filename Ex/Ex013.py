c82747b30be404147074af735475185de5b04ca4
Olegario <117552483+FeOlegario@users.noreply.github.com>
2023-06-15 17:29:53 -0300

    Update Ex013.py

--- Ex/Ex013.py
+++ Ex/Ex013.py
@@ -1,2 +1,9 @@
 # Faça um algoritmo que leia o salário de um funcionário
 # e mostre seu novo salário, com 15% de almento.
+P = float(input('Digite o seu salário: '))
+print(
+    f"""
+   Parabéns, você recebeu 15% de aumento no seu salário!
+    A gora seu salário será de R${(P + (P * 0.15)):.2f}
+    """
+)
