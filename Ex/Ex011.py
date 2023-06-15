6b3d384e551e3aa9b9a393593aa5d6b0e4f88d8d
Olegario <117552483+FeOlegario@users.noreply.github.com>
2023-06-15 16:51:41 -0300

    Update Ex011.py

--- Ex/Ex011.py
+++ Ex/Ex011.py
@@ -2,3 +2,16 @@
 # em metros, calcule a sua área e a quantidade de tinta neces
 # sária para pintá-la, sabendo que cada litro de tinta,pinta 
 # uma área de 2m².
+A = float(input('Digite a altura da parede em metros: '))
+L = float(input('Digite a largura da parede em metros: '))
+
+area = float(A * L)
+qt_tinta = float(area/2)
+
+print(
+    f"""
+    A parede possuí {A}m por {L}m.
+    Sua área é {area} m².
+    Será necessário {qt_tinta} litros para pintar toda parede.
+    """
+)
