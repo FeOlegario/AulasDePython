89c65b6ffbfd1c8670e75d9e30b25b3ca88e358a
Olegario <117552483+FeOlegario@users.noreply.github.com>
2023-06-15 17:28:12 -0300

    Update Ex012.py

--- Ex/Ex012.py
+++ Ex/Ex012.py
@@ -1,2 +1,8 @@
 # Faça um algoritmo que leia o preço de um produto
 # e mostre seu novo preço, com 5% de desconto.
+P = float(input('Digite o preço do produto: '))
+print(
+    f"""
+    O produto está com desconto de 5%, saindo por R${(P - (P * 0.05)):.2f}
+    """
+)
