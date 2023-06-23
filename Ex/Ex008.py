cc5e8b34990688c15d3859235d4f5cd3edd47cd4
FeOlegario <felipoolegario@hotmail.com>
2023-06-22 21:21:55 -0300

    novas alterações

--- Ex/Ex008.py
+++ Ex/Ex008.py
@@ -1,11 +1,16 @@
 # Escreva um programa que leia um valor em metros e 
 # o exiba convertido em centimetros e milimetros
+# Colocar as conversões em km hm dam m dm cm mm
 
 v1 = float(input('Digite um valor em metros: '))
 print(
     f"""
+    Em quilometro é: {v1/1000} km
+    Em hectómetro é: {v1/100} hm
+    Em decâmetro é: {v1/10} dam
     O valor escolhido em metros foi: {v1} m
-    E sua conversão para centímetros é: {v1*100} cm
-    Sua conversão apra milímetros é: {v1*1000} mm
+    Em decímetros é: {v1*10:.0f} dm 
+    Em centímetros é: {v1*100:.0f} cm
+    Em milímetros é: {v1*1000:.0f} mm
     """
 )
